ec777eda3ef859d27bb2d4905f86971f20c2e8ed
Tom Lemmers <515961@student.glu.nl>
2017-09-14 21:06:26 +0200

    Start of UI
    - Added fade script which can be called from outside.
    - Added loadingscreen.

---
+++ Project Google Playstore/Assets/Tom/Final/Scripts/UI/UIManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Repel
+{
+    public class UIManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] _CanvasVisuals;
+
+
+    }
+}
